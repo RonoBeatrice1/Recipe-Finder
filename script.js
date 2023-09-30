3c3ff933bc1e24badfdca586fc2eecb23fca5ece
Rono Beatrice <ronobc24@gmail.com>
2023-09-30 22:06:41 +0300

    Finalized the  CSS styles for recipe finde r

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-//Initial References
-let result = document.getElementById("result");
-let searchBtn = document.getElementById("search-btn");
-let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+let result = document.getElementById("result"); // Reference to the HTML element where the result will be displayed
+let searchBtn = document.getElementById("search-btn"); // Reference to the search button
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="; // The API endpoint for meal data
 
+// Event listener for the search button
 searchBtn.addEventListener("click", () => {
-  let userInp = document.getElementById("user-inp").value;
+  let userInp = document.getElementById("user-inp").value; // Get user input from the input field
+
+  // Check if the input field is empty
   if (userInp.length == 0) {
     result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
   } else {
+    // Fetch meal data from the API based on user input
     fetch(url + userInp)
       .then((response) => response.json())
       .then((data) => {
-        let myMeal = data.meals[0];
+        let myMeal = data.meals[0]; // Get the first meal from the API response
+
+        // Log meal details to the console for debugging
         console.log(myMeal);
         console.log(myMeal.strMealThumb);
         console.log(myMeal.strMeal);
@@ -19,6 +24,8 @@ searchBtn.addEventListener("click", () => {
         console.log(myMeal.strInstructions);
         let count = 1;
         let ingredients = [];
+
+        // Loop through meal properties to extract ingredients and measures
         for (let i in myMeal) {
           let ingredient = "";
           let measure = "";
@@ -29,8 +36,10 @@ searchBtn.addEventListener("click", () => {
             ingredients.push(`${measure} ${ingredient}`);
           }
         }
+        // Log the list of ingredients to the console
         console.log(ingredients);
 
+        // Populate the result section with meal information
         result.innerHTML = `
     <img src=${myMeal.strMealThumb}>
     <div class="details">
@@ -50,6 +59,7 @@ searchBtn.addEventListener("click", () => {
         let hideRecipe = document.getElementById("hide-recipe");
         let showRecipe = document.getElementById("show-recipe");
 
+        // Create a list of ingredients and append it to the ingredient container
         ingredients.forEach((i) => {
           let child = document.createElement("li");
           child.innerText = i;
@@ -57,9 +67,12 @@ searchBtn.addEventListener("click", () => {
           ingredientCon.appendChild(parent);
         });
 
+        // Event listener to hide the recipe section
         hideRecipe.addEventListener("click", () => {
           recipe.style.display = "none";
         });
+
+        // Event listener to show the recipe section
         showRecipe.addEventListener("click", () => {
           recipe.style.display = "block";
         });
